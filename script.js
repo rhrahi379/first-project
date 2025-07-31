6917c9664793ad68c24ecfbb2d83354defbf0938
Rahi <rhrahi379@gmail.com>
2025-07-31 14:09:30 +0600

    git changed to develop

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let count = 1;
 
 document.getElementById('plus').onclick = function () {
-    document.querySelector('h1').innerHTML = count;
+    document.querySelector('h1').innerHTML = Math.abs(count);
     count++;
 }
 document.getElementById('minus').onclick = function () {
-    document.querySelector('h1').innerHTML = count - 2;
+    document.querySelector('h1').innerHTML = Math.abs(count - 1);
     count--;
 }
